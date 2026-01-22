3977f36cd8ca70e25bd8d64a34956ea03a0cc918
Santiago <nahuelcavs21@gmail.com>
2026-01-22 12:09:46 -0300

    Cambios 1.8.4

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -12,8 +12,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 
 // Versión principal, secundaria, revisión
-[assembly: AssemblyVersion("1.8.3")]
-[assembly: AssemblyFileVersion("1.8.3")]
+[assembly: AssemblyVersion("1.8.4")]
+[assembly: AssemblyFileVersion("1.8.4")]
 
 // Opcional: para compatibilidad COM (normalmente false)
 [assembly: ComVisible(false)]
